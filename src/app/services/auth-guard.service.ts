059b69c5a23e956a907cb4873cbde4dfda7757f1
ManicAlex <alex@line.ie>
2019-03-25 04:09:27 +0000

    'lastfixupchanges'

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -14,7 +14,7 @@ export class AuthGuardService implements CanActivate {
     if (this.auth.isAuthenticated()) {
       return true;
     } else {
-      this.router.navigate(['login']);
+      this.router.navigate(['home']);
       return false;
     }
   }
